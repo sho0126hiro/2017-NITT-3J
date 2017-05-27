455f54847052b4adf9a3e29d02e550325851e3e6
sho0126hiro <sho0126hiro@yahoo.co.jp>
2017-05-27 22:49:34 +0900

    Update kadai1-1.c

--- Practice/kadai1.c/kadai1-1/kadai1-1.c
+++ Practice/kadai1.c/kadai1-1/kadai1-1.c
@@ -4,23 +4,22 @@ int main(){
 	int i,j,x;
 	int number[15];
 	for(i=0;i<10;i++){
-		printf("®”‚ğ“ü—Í=>");
+		printf("æ•´æ•°ã‚’å…¥åŠ›=>");
 		scanf("%d",&number[i]);
 	}
 	for(i=9;i>0;i--){
 		for(j=0;j<i;j++){
 			if(number[j]>number[j+1]){
-				//•À‚Ñ‘Ö‚¦
+				//ä¸¦ã³æ›¿ãˆ
 				x=number[j];
 				number[j]=number[j+1];
 				number[j+1]=x;
 			}
 		}
 	}
-	printf("========¬‚³‚¢‡=======\n");
+	printf("========å°ã•ã„é †=======\n");
 	for(i=0;i<10;i++){
 		printf(" %d",number[i]);
 	}
 	return 0;
 }
-
